9afd87187a103c887c1e72a13b3c4c2b38173f4d
Leonardo Ribeiro Leonardi - 125111359610 <125111359610@ulife.com.br>
2022-04-05 10:54:07 -0300

    Aula de Angular

---
+++ Aula05_web/paisesAPP.js
@@ -0,0 +1,5 @@
+var app = angular.module('paisesAPP', []);
+app.controller('PaisesController', function ($scope){
+$scope.capitais = {{capital: 'BRASILIA', pais: 'BRASIL'},
+{capital: 'MADRID', pais: 'ESPANHA'}};
+};
