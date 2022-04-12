dc05869ec37e7ad99c51e69c95cb913dd0c21725
Leonardo Ribeiro Leonardi - 125111359610 <125111359610@ulife.com.br>
2022-04-12 09:22:03 -0300

    Iniciando com NODE

---
+++ Aula06_web/app.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const hostname = '127.0.0.1'; //localhost
+const port = 3000; //Porta de aplicação
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200; //status retornado indicando sucesso
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Hello World</h1>');
+});
+
+server.listen(port, hostname, () => {
+    console.log('Server running at http:${hostname}:${port}/');
+});
